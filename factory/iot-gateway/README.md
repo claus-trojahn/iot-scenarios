30f41d5b2c537f0655c3ae574ab4b2a90161ba86
Claus Trojahn <claus.trojahn@outlook.com>
2017-07-22 06:04:21 +0200

    changes to gitignore

---
+++ factory/iot-gateway/README.md
@@ -0,0 +1,4 @@
+# iot-gateway
+
+Simulates a gateway that retrieves telemetry data from edge devices to the
+gateway and than further dispatches to an IoT hub.
--- factory/iot-gateway/iot-gateway.js
+++ factory/iot-gateway/iot-gateway.js
@@ -1,1 +1,25 @@
 console.log('starting iot-gateway');
+
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://test.mosquitto.org');
+
+function randomIntInc (low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+client.on('connect', function() {
+    setInterval(() => {
+        client.publish('device_telemetry', JSON.stringify({
+            source: 'device_1',
+            timestamp: Date.now(),
+            payload: randomIntInc(10,30)
+        }));
+    }, 1000);
+    //client.publish('device_telemetry', 'Hello');
+    //client.end();
+});
+
+// client.on('message', function (topic, message) {
+//     console.log('topic: ' +  topic.toString() + ' message:' + message.toString());
+//     client.end();
+// });
